5af9ee06df2f9174c8bc96387a06f8cea079bb39
Andrew ONeill <andy@evolve757.com>
2026-04-08 09:09:23 -0400

    first commit

---
+++ SportsBettingTrackerApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SportsBettingTrackerApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
