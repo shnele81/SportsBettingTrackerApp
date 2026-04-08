5e66d5aa327fb5ed0eb7c2d4f9faf40403e76958
Andrew ONeill <andy@evolve757.com>
2026-04-08 09:28:23 -0400

    created new branch for front end design of home page

--- SportsBettingTrackerApp/Program.cs
+++ SportsBettingTrackerApp/Program.cs
@@ -21,8 +21,8 @@ app.UseAuthorization();
 app.MapStaticAssets();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
