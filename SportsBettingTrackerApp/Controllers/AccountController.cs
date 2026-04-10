013a26cf6d6240b8650002189834ab61a191af63
Andrew ONeill <andy@evolve757.com>
2026-04-10 14:48:02 -0400

    Completed the Account Controller along with Login and Sign Up methods and views

---
+++ SportsBettingTrackerApp/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportsBettingTrackerApp.Controllers;
+
+public class AccountController : Controller
+{
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult SignUp()
+    {
+        return View();
+    }
+}
