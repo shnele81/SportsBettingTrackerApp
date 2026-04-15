7708acc7d59f0bd09ec121feb254f14cc5020181
Andrew ONeill <andy@evolve757.com>
2026-04-15 09:01:38 -0400

    started branch and built wager model

---
+++ SportsBettingTrackerApp/Models/WagerModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SportsBettingTrackerApp.Models;
+
+public class WagerModel
+ {
+     public int WagerId { get; set; }
+     public double WagerAmount { get; set; }
+     public double AmountReturned { get; set; }
+     public string WagerType { get; set; }
+     public string WagerSport { get; set; }
+     public DateTime WagerDate { get; set; }
+     public string WagerResult { get; set; }
+ }
