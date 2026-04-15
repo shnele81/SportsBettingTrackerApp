70f79deb6b69b87ca7ef66d9346d1cb85d80dcfb
Andrew ONeill <andy@evolve757.com>
2026-04-15 10:06:10 -0400

    Created the IWagerRepo and WagerRepo data interface and class. Finsished IWager

---
+++ SportsBettingTrackerApp/Data/IWagerRepository.cs
@@ -0,0 +1,12 @@
+﻿using SportsBettingTrackerApp.Models;
+
+namespace SportsBettingTrackerApp.Data;
+
+public interface IWagerRepository
+{
+    void InsertWager(WagerModel wagerToInsert);
+    void UpdateWager(WagerModel wagerToUpdate);
+    void DeleteWager(int WagerId);
+    IEnumerable<WagerModel> GetWagersByFilter(string? sport, string? wagerType, DateTime? date);
+    
+}
---
+++ SportsBettingTrackerApp/Data/WagerRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using SportsBettingTrackerApp.Models;
+
+namespace SportsBettingTrackerApp.Data;
+
+public class WagerRepository : IWagerRepository
+{
+    private readonly IDbConnection _connection;
+    
+    public WagerRepository (IDbConnection connection)
+    {
+        _connection = connection;
+    }
+
+    public void InsertWager(WagerModel wagerToInsert)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateWager(WagerModel wagerToUpdate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteWager(int WagerId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<WagerModel> GetWagersByFilter(string? sport, string? wagerType, DateTime? date)
+    {
+        throw new NotImplementedException();
+    }
+}
