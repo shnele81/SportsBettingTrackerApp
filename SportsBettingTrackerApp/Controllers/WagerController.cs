3915c233fb66c3e7eaf468d61fe07752be901e7b
Andrew ONeill <andy@evolve757.com>
2026-04-21 15:05:37 -0400

    Create WagerController with Index, InsertWager, and UpdateWager actions

---
+++ SportsBettingTrackerApp/Controllers/WagerController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsBettingTrackerApp.Data;
+using SportsBettingTrackerApp.Models;
+
+namespace SportsBettingTrackerApp.Controllers;
+
+public class WagerController : Controller
+{
+    private readonly IWagerRepository _wagerRepository;
+
+    public WagerController(IWagerRepository wagerRepository)
+    {
+        _wagerRepository = wagerRepository;
+    }
+
+    public IActionResult Index()
+    {
+        var wagers = _wagerRepository.GetWagersByFilter(null, null, null);
+        return View(wagers);
+    }
+
+    [HttpGet]
+    public IActionResult InsertWager()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult InsertWager(WagerModel model)
+    {
+        _wagerRepository.InsertWager(model);
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public IActionResult UpdateWager()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult UpdateWager(WagerModel model)
+    {
+        _wagerRepository.UpdateWager(model);
+        return RedirectToAction("Index");
+    }
+
+}
