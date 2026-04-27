b228ac252b400dbe8769d02ad2981286908094d5
Andrew ONeill <andy@evolve757.com>
2026-04-27 11:29:26 -0400

    change display names on user wager list

--- SportsBettingTrackerApp/Models/WagerModel.cs
+++ SportsBettingTrackerApp/Models/WagerModel.cs
@@ -1,13 +1,26 @@
-﻿namespace SportsBettingTrackerApp.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportsBettingTrackerApp.Models;
 
 public class WagerModel
  {
      public int WagerId { get; set; }
      public int UserId { get; set; }
+     [Display (Name = "Sport")]
      public string WagerSport { get; set; }
+     
+     [Display (Name = "Date of Bet")]
      public DateTime WagerDate { get; set; }
+     
+     [Display (Name = "Type of Bet")]
      public string WagerType { get; set; }
+     
+     [Display (Name = "Amount of Bet")]
      public double WagerAmount { get; set; }
+     
+     [Display (Name = "Win or Loss?")]
      public string WagerResult { get; set; }
+     
+     [Display (Name = "Amount of Money Returned")]
      public double AmountReturned { get; set; }
  }
